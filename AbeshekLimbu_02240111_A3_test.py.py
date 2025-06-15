ffc5fb6eeccd4c5ee3458e1668d1161906be14ba
Abeshek Limbu <02240111.cst@rub.edu.bt>
2025-06-15 22:15:51 +0600

    Completed Assignment A3

---
+++ AbeshekLimbu_02240111_A3_test.py.py
@@ -0,0 +1,59 @@
+# AbeshekLimbu_02240111_A3_test.py
+
+"""
+Unit Tests for Banking Application
+"""
+
+import unittest
+from AbeshekLimbu_02240111_A3 import BankAccount, InvalidInputError, TransferError
+
+class TestBankAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.acc = BankAccount("Test", 100)
+        self.other = BankAccount("Other", 50)
+
+    def test_deposit_valid(self):
+        self.acc.deposit(50)
+        self.assertEqual(self.acc.balance, 150)
+
+    def test_deposit_invalid(self):
+        with self.assertRaises(InvalidInputError):
+            self.acc.deposit(-10)
+
+    def test_withdraw_valid(self):
+        self.acc.withdraw(50)
+        self.assertEqual(self.acc.balance, 50)
+
+    def test_withdraw_invalid(self):
+        with self.assertRaises(InvalidInputError):
+            self.acc.withdraw(200)
+
+    def test_transfer_valid(self):
+        self.acc.transfer(self.other, 50)
+        self.assertEqual(self.acc.balance, 50)
+        self.assertEqual(self.other.balance, 100)
+
+    def test_transfer_invalid(self):
+        with self.assertRaises(TransferError):
+            self.acc.transfer(self.other, 200)
+
+    def test_phone_top_up_valid(self):
+        msg = self.acc.phone_top_up("1234567890", 10)
+        self.assertIn("topped up", msg)
+        self.assertEqual(self.acc.balance, 90)
+
+    def test_phone_top_up_invalid_number(self):
+        with self.assertRaises(InvalidInputError):
+            self.acc.phone_top_up("1234", 10)
+
+    def test_deposit_zero(self):
+        with self.assertRaises(InvalidInputError):
+            self.acc.deposit(0)
+
+    def test_withdraw_zero(self):
+        with self.assertRaises(InvalidInputError):
+            self.acc.withdraw(0)
+
+if __name__ == '__main__':
+    unittest.main()
